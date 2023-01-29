f2bdf777f3601f21c644f428d56722ea4b4217aa
emdelaville <123672938+emdelaville@users.noreply.github.com>
2023-01-29 13:09:34 -0500

    Update ECE_2312_Test1.m

--- ECE_2312_Test1.m
+++ ECE_2312_Test1.m
@@ -26,21 +26,39 @@ disp("Start Speaking")
 
 record(recorder, duration);
 
+
 % Wait 5 seconds
 pause(duration);
 disp("Recording over")
 
 audioArray = getaudiodata(recorder, "double");
-timeArray = audioArray/8000;
-%rescale(audioArray, 0,5);
-%plot(timeArray)
-%title("Audio")
-%xlabel("Time")
-%ylabel("Magnitude")
-
 clf
-subplot(3,1,1)
-n0 = 0;
-Time = ( 0:5 ) - n0;  % Create time axis.
-stem(Time, timeArray, 'b'), ylabel('Mag'); box('off');
-title('Audio');
+subplot(2, 1, 1)
+t = [0: length(audioArray)-1]/ Fs;
+plot(t, audioArray)
+title("Audio")
+xlabel("Time")
+ylabel("Magnitude")
+
+subplot(2, 1, 2)
+window = hamming(512);
+N_overlap = 256;
+N_fft = 1024;
+[S, F, T, P] = spectrogram(audioArray, window, N_overlap, N_fft, Fs, 'yaxis');
+figure;
+surf(T, F, 10*log10(P), 'edgecolor', 'none');
+axis tight;
+view(0,90);
+colormap(jet);
+set(gca, 'clim', [-80 -20]);
+ylim([0 8000]);
+xlabel('Time (s)');
+ylabel('Frequency (Hz)');
+
+
+
+
+
+
+
+
