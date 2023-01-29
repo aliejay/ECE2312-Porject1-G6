4b75f703d015eecb4732fff1f70594558e839d84
emdelaville <123672938+emdelaville@users.noreply.github.com>
2023-01-29 12:39:55 -0500

    Update ECE_2312_Test1.m

--- ECE_2312_Test1.m
+++ ECE_2312_Test1.m
@@ -9,13 +9,38 @@ duration = 5;
 
 recorder = audiorecorder(Fs, nBits, nChannels, 1);
 
+%pause(3);
+disp("Get Ready")
+pause(1);
+
+disp("3")
+pause(1);
+
+disp("2")
+pause(1);
+
+disp("1")
+pause(1);
+
 disp("Start Speaking")
+
 record(recorder, duration);
+
 % Wait 5 seconds
-pause(5);
+pause(duration);
 disp("Recording over")
 
-audioArray = getaudiodata(recorder);
-plot(audioArray)
-title("Audio")
+audioArray = getaudiodata(recorder, "double");
+timeArray = audioArray/8000;
+%rescale(audioArray, 0,5);
+%plot(timeArray)
+%title("Audio")
+%xlabel("Time")
+%ylabel("Magnitude")
 
+clf
+subplot(3,1,1)
+n0 = 0;
+Time = ( 0:5 ) - n0;  % Create time axis.
+stem(Time, timeArray, 'b'), ylabel('Mag'); box('off');
+title('Audio');
